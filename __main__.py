658e7ca499a93c92f09894f4f9eafd7cee256ad2
Денис <permyakov.d.a.92@mail.ru>
2026-04-29 21:44:10 +0500

    перенес чать кода в хендлерс для удобства

--- __main__.py
+++ __main__.py
@@ -8,15 +8,14 @@ import os
 from dotenv import load_dotenv, dotenv_values 
 import logging
 # importing necessary functions from telegram library
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 
 # Импортируем функции работы с БД
 from db import init_db, add_user
 
 # импорт модуля для обработки команд и сообщений
-# from handlers import unknown
+from handlers import unknown, start, echo
 
 # loading variables from .env file
 load_dotenv() 
@@ -26,35 +25,6 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 # Включаем логирование
 logging.basicConfig(format='%(asctime)s — %(name)s — %(levelname)s — %(message)s', level=logging.INFO)
-
-# Определяем функцию для обработки команды /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    # Инициализируем базу данных
-    init_db()
-    
-    # Получаем данные пользователя
-    user = update.effective_user
-    
-    # Добавляем пользователя в базу данных
-    add_user(
-        user_id=user.id,
-        username=user.username,
-        first_name=user.first_name,
-        last_name=user.last_name
-    )
-    
-    # Отправляем приветственное сообщение
-    await update.message.reply_text(f'Привет, {user.first_name}! Я твой бот. Ты успешно зарегистрирован!')
-
-# Определяем функцию для обработки текстовых сообщений
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
-
-
-async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'такой команды нет')
-    
 
 # Основная функция
 def main():
--- handlers.py
+++ handlers.py
@@ -2,6 +2,38 @@
 функции обработки разных команд и сообщений для бота
 """
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text="Sorry, I didn't understand that command.")
+from telegram import Update
+from telegram.ext import ContextTypes
+from db import init_db, add_user
+
+
+
+
+# Определяем функцию для обработки команды /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    # Инициализируем базу данных
+    init_db()
+    
+    # Получаем данные пользователя
+    user = update.effective_user
+    
+    # Добавляем пользователя в базу данных
+    add_user(
+        user_id=user.id,
+        username=user.username,
+        first_name=user.first_name,
+        last_name=user.last_name
+    )
+    
+    # Отправляем приветственное сообщение
+    await update.message.reply_text(f'Привет, {user.first_name}! Я твой бот. Ты успешно зарегистрирован!')
+
+# Определяем функцию для обработки текстовых сообщений
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'такой команды нет')
+
